5c70015bb63ed8c5f8e93a218ef7e1ef53ce288f
Stephan Holzgruber <stephan.holzgruber@gmail.com>
2023-04-18 12:58:28 +0200

    Minor improvements to helper shell scripts

--- src/molreactgen/utils/generate.sh
+++ src/molreactgen/utils/generate.sh
@@ -7,9 +7,10 @@
 GENERATE_FILE="generate.py"
 MODEL_FILE="pytorch_model.bin"
 KNOWN_FILE="../../data/prep/guacamol/csv/guacamol_v1_train.csv"
+OPTIM_FILE="scheduler.pt"
 NUM_MOLS=10000
 NUM_BEAMS=1
-BREAK_TIME=30
+BREAK_TIME=15
 
 if [ -z "$1" ]; then
   echo "No directory supplied, exit."
@@ -17,31 +18,34 @@ if [ -z "$1" ]; then
 fi
 
 if [ ! -f "$GENERATE_FILE" ]; then
-    echo "Could not find $GENERATE_FILE, exit."
-    exit
+  echo "Could not find $GENERATE_FILE, exit."
+  exit
 fi
 
 if [ ! -f "$KNOWN_FILE" ]; then
-    echo "Could not find $KNOWN_FILE, exit."
-    exit
+  echo "Could not find $KNOWN_FILE, exit."
+  exit
 fi
 
 
 echo "== Generating $NUM_MOLS molecules for each model in $1"
 echo "-- Reference file: $KNOWN_FILE"
+echo
 
-for dir in $(find "$1" -mindepth 1 -maxdepth 1 -type d); do
-    if [ -f "$dir/$MODEL_FILE" ]; then
-        echo "---- Starting generation at $(date +%T) with model in $dir"
-        echo python generate.py smiles \
-        --model "$dir" \
-        --known "$KNOWN_FILE" \
-        --num "$NUM_MOLS" \
-        --num_beams "$NUM_BEAMS" \
-        date +"---- Finished generation at %T"
-        echo "Pausing for $BREAK_TIME second(s)..."
-        sleep "$BREAK_TIME"
-    else
-        echo "-- Could not find model in $dir"
-    fi
+for dir in $(find "$1" -mindepth 0 -maxdepth 1 -type d); do
+  if [ -f "$dir/$MODEL_FILE" ] && [ ! -f "$dir/$OPTIM_FILE" ]; then
+    echo "---- Starting generation at $(date +%T) with model in $dir ..."
+    python generate.py smiles \
+    --model "$dir" \
+    --known "$KNOWN_FILE" \
+    --num "$NUM_MOLS" \
+    --num_beams "$NUM_BEAMS"
+    date +"---- Finished generation at %T"
+    echo -e "\nPausing for $BREAK_TIME second(s)...\n"
+    sleep "$BREAK_TIME"
+  elif [ -f "$dir/$OPTIM_FILE" ]; then
+    echo "-- Skipping model in $dir, seems to be a checkpoint"
+  else
+    echo "-- Could not find model in $dir"
+  fi
 done
