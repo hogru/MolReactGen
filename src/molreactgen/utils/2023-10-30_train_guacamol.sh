094e11eb38081539f25c210065a0cc78bdfce0ac
Stephan Holzgruber <stephan.holzgruber@gmail.com>
2023-11-01 14:13:27 +0100

    Add/Change experiment scripts/config

--- src/molreactgen/utils/2023-10-30_train_guacamol.sh
+++ src/molreactgen/utils/2023-10-30_train_guacamol.sh
@@ -2,7 +2,7 @@
 # Helper script to train a model n times
 
 TRAIN_FILE="train.py"
-BREAK_TIME=60
+BREAK_TIME=15
 
 if [ -z "$1" ]; then
   echo "Number of runs not supplied, exit."
@@ -20,16 +20,14 @@ for (( run=1; run<=$1; run++ ))
 do
   echo
   echo "-- Start training run $run/$1"
-#  python train.py \
-#    --config_file conf/guacamol.args \
-#    --tokenizer_name ../../tokenizers/char_unigram_176 \
-#    --learning_rate 0.0025 \
-#    --per_device_train_batch_size 64 \
-#    --random_seed true
   python train.py \
-    --config_file conf/uspto50k.args \
-    --pre_tokenizer smarts \
-    --algorithm wordlevel \
+    --config_file conf/guacamol.args \
+    --tokenizer_name ../../tokenizers/guacamol/char_wordpiece_176 \
     --random_seed true
+#   python train.py \
+#     --config_file conf/uspto50k.args \
+#     --pre_tokenizer smarts \
+#     --algorithm wordlevel \
+#     --random_seed true
   sleep "$BREAK_TIME"
 done
